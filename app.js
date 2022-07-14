99ae57b8c3c001c8a1ef7132ec8c238b00653ed0
Janish Nehyan <85396611+Janishnehyan03@users.noreply.github.com>
2022-07-14 21:58:10 +0530

    mongo uri removed from .env and  changed to localhost

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, (err) => {
+mongoose.connect("mongodb://localhost:27017/graphql", (err) => {
   if (!err) console.log("DB connected");
 });
 
@@ -22,10 +22,13 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`server running on port ${process.env.PORT || 5000}`);
